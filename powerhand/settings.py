f27a8fb62afc6f70e7a85d1d444c235c90cc38c9
MairoPedroIsaac <mairopedroisaac@gmail.com>
2026-03-09 23:39:16 +0200

    Switch to PostgreSQL with dj-database-url

--- powerhand/settings.py
+++ powerhand/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +64,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'powerhand.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 STATIC_URL = '/static/'
