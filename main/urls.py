1c32c54747fa662abf8f9b036acf384b9b918521
Mairo Pedro Isaac <mairopedroisaac@gmail.com>
2026-08-26 22:38:05 +0200

    fix: remove sample work drag drop, fix consent box, and hide portfolio links

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about, name='about'),
     path('solutions/', views.solutions, name='solutions'),
-    path('portfolio/', views.portfolio_view, name='portfolio'),
-    path('portfolio/<slug:slug>/', views.portfolio_detail_view, name='portfolio_detail'),
+    # path('portfolio/', views.portfolio_view, name='portfolio'),
+    # path('portfolio/<slug:slug>/', views.portfolio_detail_view, name='portfolio_detail'),
     path('contact/', views.contact_view, name='contact'),
     path('collective/', views.collective, name='collective'),
 ]
